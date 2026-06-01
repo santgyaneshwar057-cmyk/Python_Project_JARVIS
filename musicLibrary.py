9e159b9ba64f310a7652bc0ccbf08210f6f40766
santgyaneshwar057-cmyk <santgyaneshwar057@gmail.com>
2026-06-01 13:12:21 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "born" : "https://youtu.be/dCmp56tSSmA?si=4DWviKXt0WpyYfy2",
+    "dil" : "https://youtu.be/W_ZX2gSIqmw?si=rNyJey-7xMInS74o",
+    "Ishq" : "https://youtu.be/CT9n1akmfTo?si=5pG5wIsxiSQvvazt",
+    "Vaaroon" : "https://youtu.be/BrKnJw82Lus?si=m7nvTqfQ8-0bmgOL",
+    "wavy" : "https://youtu.be/fO6908Js_18?si=MkwyLA5HTi-kd0la"
+}
